2d0eca68f6dca99cafc577bc8bc22f63f0893ec5
Clement CHAMEYRAT <cchameyr@e2r11p19.42.fr>
2018-06-11 16:46:23 +0200

    auto save

--- ft_bzero.s
+++ ft_bzero.s
@@ -1,37 +1,13 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    Makefile                                           :+:      :+:    :+:    #
+#    ft_bzero.s                                         :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2018/06/11 12:08:35 by cchameyr          #+#    #+#              #
-#    Updated: 2018/06/11 16:46:20 by cchameyr         ###   ########.fr        #
+#    Created: 2018/06/11 16:44:32 by cchameyr          #+#    #+#              #
+#    Updated: 2018/06/11 16:44:34 by cchameyr         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-.PHONY: all clean fclean re
 
-SRCS = ft_bzero.s
-
-ASM = nasm
-
-N_FLAGS = -f macho64 -DOSX
-
-LD = ld
-
-LD_FLAGS = -axosx_version_min 10.8 -lSystem
-
-FLAGS = -Wall -Wextra -Werror
-
-NAME = libfts.a
-
-all: $(NAME)
-
-$(NAME): $(OBJS)
-
-
-$(OBJS):
-	$(ASM) $(N_FLAGS) $(SRCS)
-
-clean: 
