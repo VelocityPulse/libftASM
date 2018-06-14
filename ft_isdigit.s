0b268c61e0f32c55430203fc7196ca698b4d0e20
Clement CHAMEYRAT <cchameyr@e2r11p19.42.fr>
2018-06-14 11:10:17 +0200

    work on ft_isdigit

--- ft_isdigit.s
+++ ft_isdigit.s
@@ -1,12 +1,18 @@
-
-section .data
-
 section .text
-global _main
+global ft_isdigit
 
-_main:
-push rbp
-mov rbp, rsp
+ft_isdigit:
+	push rbp
+	mov rbp, rsp
+	cmp rdi, 48
+	jl false
+	cmp rdi, 57
+	jg false
+	mov rax, 1
+	leave
+	ret
 
-leave
-ret
+false:
+	mov rax, 0
+	leave
+	ret
