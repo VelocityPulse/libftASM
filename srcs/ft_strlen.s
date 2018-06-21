913e61753596d25f8f93028c64c4aa53da3bd303
Clement CHAMEYRAT <cchameyr@e2r10p19.42.fr>
2018-06-21 16:03:55 +0200

    rework and debug strlen

--- srcs/ft_strlen.s
+++ srcs/ft_strlen.s
@@ -4,13 +4,16 @@ global _ft_strlen
 _ft_strlen:
 	push rbp
 	mov rbp, rsp
-	mov ecx, -1
-	mov al, 0
-	cld
-	repne scasb
-	not ecx
+	cmp rdi, 0 ; test if rdi is null
+	je exit
+	mov ecx, -1 ; set ecx to maximal bit value
+	mov al, 0 ; set the char to research
+	cld ; init direction flag
+	repne scasb ; start the research on the rdi register
+	not ecx ; reverse ecx
+	lea rax, [ecx - 1] ; get value of ecx without the /0 count
+	sub rdi, rax ; reset rdi which was auto increment by repne scasbi
 
 	exit:
-		lea rax, [ecx - 1]
 		leave
 		ret
